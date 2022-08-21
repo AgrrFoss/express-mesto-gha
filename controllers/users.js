4fdad513b224846a98b7b66d42dccc387a2b4212
Gregori Orlov <Orlov.Gregori@yandex.ru>
2022-08-21 23:41:44 +0300

    обновить после еще 2х попыток решить проблему

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,9 @@
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
-  console.log(req);
+  User.find({})
+    .then(users => res.send({ data: users }))
+    .catch(err => res.status(500).send({ message: err.message }));
 };
 /*
 module.exports.getUser = (req, res) => {
@@ -9,9 +11,9 @@ module.exports.getUser = (req, res) => {
 };
 */
 module.exports.createUser = (req, res) => {
-  console.log(req);
-  const data = req.body;
-  User.create(data)
+  console.log(req.body);
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
     .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 
